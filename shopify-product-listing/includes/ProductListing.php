0ca094e1ef846e67ad94818ef6568555aae59ef8
Mahmudul Hasan <80215312+mahmudulhasan716@users.noreply.github.com>
2024-04-23 17:31:38 +0600

    product add updated

--- shopify-product-listing/includes/ProductListing.php
+++ shopify-product-listing/includes/ProductListing.php
@@ -2,7 +2,7 @@
 
 namespace WeLabs\ShopifyProductListing;
 
-use WeDevs\Dokan\Product\Manager as ProductManager;
+// use WeDevs\Dokan\Product\Manager as ProductManager;
 
 // use function \media_sideload_image;
 
@@ -11,8 +11,8 @@ use WeDevs\Dokan\Product\Manager as ProductManager;
 // require_once(ABSPATH . 'wp-admin/includes/image.php');
 
 // use Shopify\Clients\Rest;
-
-class ProductListing extends ProductManager   {
+// extends ProductManager
+class ProductListing  {
 
     public function __construct(){
         // add_action( 'init', [$this, 'shopify_product_add'] );
@@ -26,15 +26,15 @@ class ProductListing extends ProductManager   {
     }
 
 
-function custom_woocommerce_settings_tab($settings_tabs) {
-    $settings_tabs['custom_tab'] = 'Shopify';
-    return $settings_tabs;
-}
+    function custom_woocommerce_settings_tab($settings_tabs) {
+        $settings_tabs['custom_tab'] = 'Shopify';
+        return $settings_tabs;
+    }
 
- function custom_woocommerce_settings_content() {
-    // Add your custom settings content here
-    echo '<h2>Shopify Access </h2>';
-    ?>
+    function custom_woocommerce_settings_content() {
+        // Add your custom settings content here
+        echo '<h2>Shopify Access </h2>';
+        ?>
 <label for="shopify_shop_url"> Shop Url </label>
 <input type="text" id="shopify_shop_url" name="shopify_shop_url"
     value="<?php echo esc_attr(get_option('shopify_shop_url')); ?>" /> </br> </br>
@@ -49,9 +49,9 @@ function custom_woocommerce_settings_tab($settings_tabs) {
 
 <?php
 
- }
+    }
 
- public function save_custom_woocommerce_settings() {
+    public function save_custom_woocommerce_settings() {
         // Save custom settings to the database
     if ( isset( $_POST['shopify_shop_url'] ) ) {
         update_option( 'shopify_shop_url', sanitize_text_field( $_POST['shopify_shop_url'] ) );
@@ -67,26 +67,26 @@ function custom_woocommerce_settings_tab($settings_tabs) {
 
 
 
-function my_custom_menu_page() {
-    add_menu_page(
-        'Wc Data Listing',    // Page title
-        'Shopify to Wc',    // Menu title
-        'manage_options', // Capability required to access the menu page
-        'my-custom-menu', // Menu slug
-        array($this, 'my_custom_menu_page_content'), // Callback function to display the menu page content
-        'dashicons-admin-generic', // Icon URL or Dashicons class
-    );
-}
+    function my_custom_menu_page() {
+        add_menu_page(
+            'Wc Data Listing',    // Page title
+            'Shopify to Wc',    // Menu title
+            'manage_options', // Capability required to access the menu page
+            'my-custom-menu', // Menu slug
+            array($this, 'my_custom_menu_page_content'), // Callback function to display the menu page content
+            'dashicons-admin-generic', // Icon URL or Dashicons class
+        );
+    }
 
     function my_custom_menu_page_content() {
-       ?>
+        ?>
 <div class="wrap">
     <h2>My Custom Menu Page</h2>
     <p>This is where you can add your custom content.</p>
     <a href="?page=my-custom-menu&action=shopify_product_add" class="button">Store</a>
 </div>
 <?php
-}
+    }
 
      function process_form_submission() {
         if (isset($_GET['action']) && $_GET['action'] === 'shopify_product_add') {
@@ -98,83 +98,95 @@ function my_custom_menu_page() {
     public function shopify_product_add(){
         
        $shopify = \PHPShopify\ShopifySDK::config([
-        'ShopUrl' => "'" . esc_attr(get_option('shopify_shop_url')) . "'",
-    'AccessToken' => "'" . esc_attr(get_option('shopify_access_token')) . "'",
-    'ApiVersion' => "'" . esc_attr(get_option('shopify_api_version')) . "'",
+        // 'ShopUrl' => "'" . esc_attr(get_option('shopify_shop_url')) . "'",
+        // 'AccessToken' => "'" . esc_attr(get_option('shopify_access_token')) . "'",
+        // 'ApiVersion' => "'" . esc_attr(get_option('shopify_api_version')) . "'",
     
-]);
+        ]);
 
-$products = $shopify->Product->get();
+   $products = $shopify->Product->get();
 
-foreach($products as $product){
+    foreach($products as $product){
+    $image_url = $product['images'][0]['src'];
+    $product_price = $product['variants'][0]['price'];
+    $sale_price = $product['variants'][0]['compare_at_price'];
 
-$image_url = $product['images'][0]['src'];
+    $new_product = array(
+    'post_title' =>sanitize_text_field($product['title']),
+    'post_content' => sanitize_text_field ($product['body_html']),
+    'regular_price' => $product_price,
+    'stock' => '',
+    'sale_price' => $sale_price,
+    'thumbnail_url' => $image_url,
+    'post_status' => 'publish',
+    'post_type' => 'product',
+    );
+    
+    // $new_product = array(
+    // 'name' =>sanitize_text_field($product['title']),
+    // 'regular_price' => $product['variants'][0]['price'],
+    // 'featured_image_id' => $this->get_thumbnail_id($image_url),
+    // 'post_content' => sanitize_text_field ($product['body_html']),
+    // 'status' => 'draft',
+    // 'post_type' => 'product',
+    // );
 
-// $new_product = array(
-// 'post_title' =>sanitize_text_field($product['title']),
-// 'post_content' => sanitize_text_field ($product['body_html']),
-// 'post_status' => 'publish',
-// 'post_type' => 'product',
-// );
-$new_product = array(
-'name' =>sanitize_text_field($product['title']),
-'regular_price' => $product['variants'][0]['price'],
-'featured_image_id' => $this->get_thumbnail_id($image_url),
-'status' => 'draft',
-// 'post_type' => 'product',
-);
+    $product = $this->create($new_product);
+    }
+    }
 
+    public function create($args = []){
 
-$product = $this->create($new_product);
+        global $wpdb;
+        
+        $wpdb->insert(
+            $wpdb->posts,
+            array(
+                'post_title' => $args['post_title'],
+                'post_content' =>$args['post_content'],
+                'post_status' => 'publish',
+                'post_type' => 'product',
+            )
+        );
 
-// $woocommerce_product_id = wp_insert_post($new_product);
+        // $product_id = wp_insert_post($new_product);
 
-// $product_thumbnail_ids = array();
+        $product_id = $wpdb->insert_id;
 
-// foreach ($product['images'] as $image) {
+        update_post_meta($product_id, '_regular_price', $args['regular_price']);
+        update_post_meta($product_id, '_price', $args['sale_price']);
+        update_post_meta($product_id, '_stock', $args['stock']);
+        // update_post_meta($product_id, '_sku', $sku);
 
-// $image_url = $image['src'];
+        if ($args['thumbnail_url']) {
+        $thumbnail_id = media_sideload_image($args['thumbnail_url'], $product_id, '', 'id');
+        if (!is_wp_error($thumbnail_id)) {
+            set_post_thumbnail($product_id, $thumbnail_id);
+        }
+    }
+        // $wpdb->insert(
+        //     $wpdb->postmeta,
+        //     array(
+        //         'post_id' => $product_id,
+        //          'meta_key' => '_regular_price'
+        //         'value' => '',
+        //         
+        //     )
+        // );
+        
+    }
 
-// $image_id = media_sideload_image($image_url, $woocommerce_product_id, $product['title']);
+    public function get_thumbnail_id($image) {
 
+    $attachment = new Attachment();
 
-// if (!is_wp_error($image_id) && !empty($image_id)) {
-// $product_thumbnail_ids[] = $image_id;
-// }
-// }
-
-
-// if ($product_thumbnail_ids) {
-// set_post_thumbnail($woocommerce_product_id, $product_thumbnail_ids[0]);
-// }
-// update_post_meta($woocommerce_product_id, '_product_image_gallery', implode(',', $product_thumbnail_ids));
-
-
-// update_post_meta($woocommerce_product_id, '_regular_price', $price);
-// update_post_meta($woocommerce_product_id, '_price', $price);
-
-}
-
-}
-
-
-public function get_thumbnail_id($image) {
-
-
-$attachment = new Attachment();
-
-try {
-$id = $attachment->get_attachment_id_from_url( $image, 0 );
-} catch ( \Throwable $th ) {
-return 0;
-}
-
-return $id;
-}
-
-
-
-
+    try {
+        $id = $attachment->get_attachment_id_from_url( $image, 0 );
+    } catch ( \Throwable $th ) {
+        return 0;
+    }
+    return $id;
+    }
 
 }
 
